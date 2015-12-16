829137c3470b16404bfa3076bdcaf9c13e811e66
michaelfortuna <m@ninjastack.com>
2015-12-16 11:16:49 -0500

    Added lg buy buttons, added owl carousel demo.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 $( document ).ready(function() {
 
-
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
+});
 
   $('.nav-toggle').click(function() {
     $('nav').slideToggle();
